a4a51e937c74e4593ae73d59df255990aa8c4979
DarahLeite <93683162+DarahLeite@users.noreply.github.com>
2024-01-15 11:53:44 -0300

    Projeto de estudos do Curso Python

---
+++ Projeto/aula1.py
@@ -0,0 +1,21 @@
+'''print(123, end="love")
+print(13, 23, sep="")
+print('-----------------------------------')
+print()
+print()
+
+
+print("1", type(float("1")))
+'''
+
+
+print("Vamos verificar a idade do cara?")
+idade= 95
+print(idade)
+idade_minima= 18
+idade_avancada= 65
+menor_de_idade= idade<18
+velho= idade>idade_avancada
+
+print("Ele é menor de idade?", menor_de_idade)
+print("Ele é Veio?", velho)
